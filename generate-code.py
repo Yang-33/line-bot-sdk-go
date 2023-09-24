d255412be3fd51cf242eec1f2dd5f85e85a0340e
Yuta Kasai <kasai.yuta0810@gmail.com>
2023-09-24 15:52:53 +0900

    generator

---
+++ generate-code.py
@@ -0,0 +1,83 @@
+import os
+import subprocess
+import sys
+
+def run_command(command):
+    print(command)
+    proc = subprocess.run(command, shell=True, text=True, capture_output=True)
+
+    if len(proc.stderr) != 0:
+        print("\n\nSTDERR:\n\n")
+        print(proc.stderr)
+        print("\n\n")
+
+    if len(proc.stdout) != 0:
+        print("\n\nSTDOUT:\n\n")
+        print(proc.stdout)
+
+    if proc.returncode != 0:
+        print(f"\n\nCommand '{command}' returned non-zero exit status {proc.returncode}.")
+        sys.exit(1)
+
+    return proc.stdout.strip()
+
+
+def main():
+    os.chdir("generator")
+    run_command('mvn package -DskipTests=true')
+    os.chdir("..")
+
+    # components = [
+    #     {"sourceYaml": "messaging-api.yml"},
+    #     {"sourceYaml": "channel-access-token.yml"},
+    #     {"sourceYaml": "insight.yml"},
+    #     {"sourceYaml": "liff.yml"},
+    #     {"sourceYaml": "manage-audience.yml"},
+    #     {"sourceYaml": "module-attach.yml"},
+    #     {"sourceYaml": "module.yml"},
+    #     {"sourceYaml": "shop.yml"},
+    # ]
+
+    # for component in components:
+    #     sourceYaml = component['sourceYaml']
+    #     outputPath = 'lib/' + sourceYaml.replace('.yml', '')
+
+    #     run_command(f'rm -rf {outputPath}/')
+
+    #     command = f'''java \\
+    #                 -cp ./tools/openapi-generator-cli.jar:./generator/target/line-bot-sdk-nodejs-generator-openapi-generator-1.0.0.jar \\
+    #                 org.openapitools.codegen.OpenAPIGenerator \\
+    #                 generate \\
+    #                 -g line-bot-sdk-nodejs-generator \\
+    #                 -o {outputPath} \\
+    #                 -i line-openapi/{sourceYaml} \\
+    #               '''
+    #     # --global-property modelDocs=false \ \
+    #       # --additional-properties=packageVersion={package_version}
+    #     run_command(command)
+        ## webhook requires only models.
+
+
+    ## TODO: We'd like to specify apis=false but __init__.py is deleted. Fix it.
+    sourceYaml = "webhook.yml"
+    modelPackage = "linebot2"
+    modelPackagePath = modelPackage.replace(".", "/")
+
+    run_command(f'rm -rf {modelPackagePath}/')
+
+    command = f'''java \\
+                -cp ./tools/openapi-generator-cli.jar:./generator/target/line-bot-sdk-go-generator-openapi-generator-1.0.0.jar \\
+                org.openapitools.codegen.OpenAPIGenerator \\
+                generate \\
+                -g line-bot-sdk-go-generator \\
+                -o . \\
+                --global-property modelDocs=false,apiDocs=false \\
+                --additional-properties=excludeText=true \\
+                --additional-properties=generateSourceCodeOnly=true \\
+                --package-name {modelPackage} \\
+                -i line-openapi/{sourceYaml}
+              '''
+    run_command(command)
+
+if __name__ == "__main__":
+    main()
